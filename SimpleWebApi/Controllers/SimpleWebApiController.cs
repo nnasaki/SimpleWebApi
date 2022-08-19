313b06fe6a78063f01bdf99204774209166bd2ff
Masaki Yamamoto <masaki.yamamoto@sitecore.com>
2022-08-19 15:55:03 +0900

    Add project files.

---
+++ SimpleWebApi/Controllers/SimpleWebApiController.cs
@@ -0,0 +1,21 @@
+﻿using Sitecore.Services.Infrastructure.Web.Http;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace SimpleWebApi.Controllers
+{
+    // TODO 本番環境ではCORSの設定を必ず絞ってください。これは全許可となっています。
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
+    public class SimpleWebApiController : ServicesApiController
+    {
+        public SimpleWebApiController()
+        {
+        }
+
+        [HttpGet]
+        public string Stats()
+        {
+            return "ok SimpleWebApi";
+        }
+    }
+}
---
+++ SimpleWebApi/Pipelines/RegisterHttpRoutes.cs
@@ -0,0 +1,23 @@
+﻿using System.Web.Http;
+using Sitecore.Pipelines;
+
+namespace SimpleWebApi.Pipelines
+{
+    public class RegisterHttpRoutes
+    {
+        public void Process(PipelineArgs args)
+        {
+            GlobalConfiguration.Configure(Configure);
+        }
+        protected void Configure(HttpConfiguration configuration)
+        {
+            var routes = configuration.Routes;
+            routes.MapHttpRoute("SimpleWebApi.stats", "api/simplewebapi/stats", new
+            {
+                controller = "SimpleWebApi",
+                action = "Stats"
+            });
+
+        }
+    }
+}
